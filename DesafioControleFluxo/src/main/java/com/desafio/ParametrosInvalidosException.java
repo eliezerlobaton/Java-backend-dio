5e900b2b060d18761cc12f2d1a1a1c01268153f5
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2024-06-05 07:30:33 -0300

    feat: desafio pronto

---
+++ DesafioControleFluxo/src/main/java/com/desafio/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package DesafioControleFluxo.src.main.java.com.desafio;
+
+public class ParametrosInvalidosException extends Exception {
+  public ParametrosInvalidosException(String mensagem) {
+    super(mensagem);
+  }
+}
