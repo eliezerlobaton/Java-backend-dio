f6c71e7eb9b0efdb0556593b3313447ff7d7db8f
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2024-06-05 07:35:48 -0300

    restore

--- DesafioControleFluxo/src/main/java/com/desafio/ParametrosInvalidosException.java
+++
@@ -1,7 +0,0 @@
-package DesafioControleFluxo.src.main.java.com.desafio;
-
-public class ParametrosInvalidosException extends Exception {
-  public ParametrosInvalidosException(String mensagem) {
-    super(mensagem);
-  }
-}
