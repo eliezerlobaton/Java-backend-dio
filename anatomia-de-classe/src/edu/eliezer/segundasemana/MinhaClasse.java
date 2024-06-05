72b3b031a633f9f750b49399cb286d933f7dbebd
Eliezer Lobaton <eliezerlobaton39@gmail.com>
2024-06-05 02:21:17 -0300

    praticas

--- anatomia-de-classe/src/edu/eliezer/segundasemana/MinhaClasse.java
+++
@@ -1,9 +0,0 @@
-package edu.eliezer.segundasemana;
-
-public class MinhaClasse {
-  public static void main(String[] args) {
-    String nome = "João";
-    int idade = 25;
-    System.out.println("Olá, meu nome é " + nome + " e tenho " + idade + " anos.");
-  }
-}
